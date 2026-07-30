e3fd1abfdd4306e114fd3a307ecdb45451c4ee13
jj55-aa <3525200960@qq.com>
2026-07-30 15:02:23 +0800

    pure Java, no Kotlin deps

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.jj55_aa.info_cards.widget"
         minSdk = 23
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
     }
 
     buildTypes {
@@ -19,13 +19,4 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
-dependencies {
-    // org.json is built into Android SDK
 }
