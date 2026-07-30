b9fddb1d8725761da8820dfcec6defe2a6dac4fb
jj55-aa <3525200960@qq.com>
2026-07-30 09:44:43 +0800

    fix: add enableNotifications: false to TwaManifest to prevent Gradle parse error

--- .github/scripts/init-twa.cjs
+++ .github/scripts/init-twa.cjs
@@ -19,6 +19,7 @@ const twa = new TwaManifest({
   webManifestUrl: 'https://jj55-aa.github.io/info-cards/manifest.json',
   iconUrl: 'https://jj55-aa.github.io/info-cards/icon-512.png',
   maskableIconUrl: 'https://jj55-aa.github.io/info-cards/icon-512.png',
+  enableNotifications: false,
   signingKey: { path: './debug.keystore', alias: 'androiddebugkey', password: 'android' },
 });
 
