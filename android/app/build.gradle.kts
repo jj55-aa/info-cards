c4316683ba81d021b17f3e12c567b272235550a0
jj55-aa <3525200960@qq.com>
2026-07-30 11:14:49 +0800

    add native Android widget project

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "io.github.jj55_aa.info_cards"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "io.github.jj55_aa.info_cards"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 2
+        versionName = "2.0"
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.glance:glance-appwidget:1.1.1")
+    implementation("androidx.core:core-ktx:1.15.0")
+}
